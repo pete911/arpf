b20b0ea2f8a915c83a0df5ff55f6206901882827
pete911 <p.reisinger@gmail.com>
2022-04-18 12:40:15 +0100

    simplify ip functions

--- internal/ip.go
+++ internal/ip.go
@@ -6,24 +6,20 @@ import (
 )
 
 func BroadcastIPv4(ip *net.IPNet) net.IP {
-	maskOnes, _ := ip.Mask.Size()
-	mask := uint32(0xFFFFFFFF >> maskOnes)
-	out := binary.BigEndian.Uint32(ip.IP.To4()) | mask
+	out := ipToInt(ip.IP) | getMask(ip)
 	return intToIP(out)
 }
 
 func HostMinIPv4(ip *net.IPNet) net.IP {
-	maskOnes, _ := ip.Mask.Size()
-	mask := uint32(0xFFFFFFFF >> maskOnes)
 	// network address
-	out := binary.BigEndian.Uint32(ip.IP.To4()) &^ mask
+	out := ipToInt(ip.IP) &^ getMask(ip)
 	// increment by one
 	out++
 	return intToIP(out)
 }
 
 func HostMaxIPv4(ip *net.IPNet) net.IP {
-	bcastInt := binary.BigEndian.Uint32(BroadcastIPv4(ip).To4())
+	bcastInt := ipToInt(BroadcastIPv4(ip))
 	bcastInt--
 	return intToIP(bcastInt)
 }
@@ -36,16 +32,22 @@ func CIDRIPs(ip *net.IPNet) []net.IP {
 // IPs returns list of IPs including from and to arguments
 func IPs(from net.IP, to net.IP) []net.IP {
 	var out []net.IP
-	for i := binary.BigEndian.Uint32(from.To4()); i <= binary.BigEndian.Uint32(to.To4()); i++ {
-		b := make([]byte, 4)
-		binary.BigEndian.PutUint32(b, i)
-		out = append(out, b)
+	for i := ipToInt(from); i <= ipToInt(to); i++ {
+		out = append(out, intToIP(i))
 	}
 	return out
+}
+
+func getMask(ip *net.IPNet) uint32 {
+	return 0xFFFFFFFF ^ binary.BigEndian.Uint32(ip.Mask)
 }
 
 func intToIP(i uint32) net.IP {
 	b := make([]byte, 4)
 	binary.BigEndian.PutUint32(b, i)
 	return b
+}
+
+func ipToInt(ip net.IP) uint32 {
+	return binary.BigEndian.Uint32(ip.To4())
 }
